4376576544cea32f9b2f560860111ed5906a6df0
kaengreg <kaengreg@kaengregs-MacBook.local>
2024-04-26 22:30:24 +0300

    models added

--- mmlu_ru.py
+++ mmlu_ru.py
@@ -150,7 +150,8 @@ def load_saiga_model(model_id: str) -> tp.Tuple:
 def load_model_components(model_id: str) -> tp.Tuple:
     llama_models = ["huggyllama/llama-7b", "huggyllama/llama-13b", "huggyllama/llama-30b", "huggyllama/llama-65b",
                     "TheBloke/Llama-2-7B-fp16", "TheBloke/Llama-2-13B-fp16", "TheBloke/Llama-2-70B-fp16", "/content/models/Llama2-7b-cropped",
-                    "NousResearch/Meta-Llama-3-8B", "/content/models/Llama3-8b-cropped"]
+                    "NousResearch/Meta-Llama-3-8B", "/content/models/Llama3-8b-cropped_8", "/content/models/Llama3-8b-cropped_4",
+                    "/content/models/Llama3-8b-cropped_2"]
     saiga_models = ["IlyaGusev/saiga_7b_lora", "IlyaGusev/saiga_13b_lora", "IlyaGusev/saiga_30b_lora", "IlyaGusev/saiga_65b_lora"]
 
     if model_id in llama_models:
